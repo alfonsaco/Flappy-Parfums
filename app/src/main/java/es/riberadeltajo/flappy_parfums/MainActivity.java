f184a5c8b37b498f86cb8ba18e9c66b690587e82
alfonsaco <alfonso.rincondev@gmail.com>
2025-02-23 00:24:39 +0100

    Se crea la transición entre actividade + Elementos de inicio

--- app/src/main/java/es/riberadeltajo/flappy_parfums/MainActivity.java
+++ app/src/main/java/es/riberadeltajo/flappy_parfums/MainActivity.java
@@ -1,6 +1,13 @@
 package es.riberadeltajo.flappy_parfums;
 
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
+import android.content.Intent;
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +17,52 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    // boton de prueba
+    private Button btnPlay;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        btnPlay=findViewById(R.id.btnPlay);
+
+        btnPlay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this, JuegoActivity.class);
+                startActivity(intent);
+                overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
+            }
+        });
+
+        animarColonia();
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    private void animarColonia() {
+        // Agregar frames del personaje
+        AnimationDrawable animacionColonia;
+        ImageView personaje=(ImageView) findViewById(R.id.imgPersonaje);
+        personaje.setBackgroundResource(R.drawable.personaje1);
+
+        animacionColonia=(AnimationDrawable) personaje.getBackground();
+        animacionColonia.start();
+
+        // Agregar efectos al personaje
+        AnimatorSet set=new AnimatorSet();
+        ObjectAnimator volar=ObjectAnimator.ofFloat(personaje, "translationY", -20f, 20f);
+        volar.setDuration(400);
+        volar.setRepeatCount(ObjectAnimator.INFINITE);
+        volar.setRepeatMode(ObjectAnimator.REVERSE);
+
+        set.play(volar);
+        set.start();
     }
 }
